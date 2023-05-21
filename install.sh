fb147cc147f94676538bccc2bd0f4a10136145c9
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-05-21 20:41:36 +0530

    Minor Changes

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-dnld_url="https://raw.githubusercontent.com/Arkapravo-Ghosh/dnld/main/dnld.sh"
+dnld_url="https://raw.githubusercontent.com/Arkapravo-Ghosh/down/main/down.sh"
 install_dir="/data/data/com.termux/files/usr/bin"
 install_file="down"
 curl -sSL "$dnld_url" -o "$install_dir/$install_file"
@@ -8,5 +8,5 @@ echo "The 'down' command has been installed in $install_dir"
 if [[ $(command -v down) ]]; then
     echo "Please restart your shell or run 'source ~/.bashrc' to start using the 'down' command."
 else
-    echo "You can start using the 'down -h' command immediately."
+    echo "You can start using the 'down' command immediately."
 fi
