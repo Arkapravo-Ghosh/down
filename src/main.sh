ac514da962d54ec12a5ddfddf3eb13d03672515a
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-05-21 20:57:17 +0530

    Fixed URL Checking

--- src/main.sh
+++ src/main.sh
@@ -5,7 +5,7 @@ display_help() {
     echo "  -h, --help       Display this help message"
     echo "  -u, --url URL    Specify the share URL of the video"
     echo ""
-    echo "down v1.0.2 Made by Arkapravo Ghosh"
+    echo "down v1.0.3 Made by Arkapravo Ghosh"
 }
 download_video() {
     video_url=$(yt-dlp --no-playlist --get-url "$1")
@@ -57,6 +57,8 @@ if [[ -z $share_url ]]; then
     read -p "Enter URL: " share_url
 fi
 if [[ $share_url == *"youtube.com"* ]]; then
+    download_video "$share_url"
+elif [[ $share_url == *"youtu.be"* ]]; then
     download_video "$share_url"
 elif [[ $share_url == *"instagram.com"* ]]; then
     download_video "$share_url"
