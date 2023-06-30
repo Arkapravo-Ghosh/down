9030ee8244038ad1ac71d3a8d0b67705da020ffd
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-06-30 20:46:56 +0530

    feat: added support for youtube

--- src/main.sh
+++ src/main.sh
@@ -5,18 +5,27 @@ display_help() {
     echo "  -h, --help       Display this help message"
     echo "  -u, --url URL    Specify the share URL of the video"
     echo ""
-    echo "down v1.0.4 Made by Arkapravo Ghosh"
+    echo "down v1.0.5 Made by Arkapravo Ghosh"
 }
 download_video() {
-    video_url=$(yt-dlp --no-playlist --get-url "$1")
-    if [[ -n $video_url ]]; then
-        filename=$(basename "$video_url" | cut -d'?' -f1)
+    if [[ $video_url == *"instagram.com"* ]]; then
+        video_url=$(yt-dlp --no-playlist --get-url "$1")
+        if [[ -n $video_url ]]; then
+            filename=$(basename "$video_url" | cut -d'?' -f1)
+            download_dir="$HOME/storage/downloads"
+            wget -O "$download_dir/$filename" "$video_url"
+            exiftool -overwrite_original -FileModifyDate="$(date +"%Y:%m:%d %H:%M:%S")" "$download_dir/$filename"
+            am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://$download_dir/$filename"
+        else
+            echo "Failed to retrieve video URL."
+        fi
+    elif [[ $video_url == *"youtube.com"* ]] || [[ $video_url == *"youtu.be"* ]]; then
+        yt-dlp --merge-output-format mp4 -f "bestvideo+bestaudio[ext=m4a]/best" "$1"
         download_dir="$HOME/storage/downloads"
-        wget -O "$download_dir/$filename" "$video_url"
-        exiftool -overwrite_original -FileModifyDate="$(date +"%Y:%m:%d %H:%M:%S")" "$download_dir/$filename"
-        am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://$download_dir/$filename"
+        mv *.mp4 "$download_dir"
+        am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://$download_dir"
     else
-        echo "Failed to retrieve video URL."
+        echo "Unsupported URL domain."
     fi
 }
 if [[ $1 == "-h" || $1 == "--help" ]]; then
@@ -56,9 +65,4 @@ done
 if [[ -z $share_url ]]; then
     read -p "Enter URL: " share_url
 fi
-if [[ $share_url == *"instagram.com"* ]]; then
-    download_video "$share_url"
-else
-    echo "Unsupported URL. Only YouTube and Instagram URLs are supported."
-    exit 1
-fi
+download_video "$share_url"
